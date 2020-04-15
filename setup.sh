8c5290507300d56515b652011d4632c0eee20754
Darkbladecr <smig88@gmail.com>
2020-04-15 10:08:08 +0100

    move chown to end

--- setup.sh
+++ setup.sh
@@ -7,14 +7,13 @@ LABELS=src:glances
 groupadd --system glances
 useradd -s /sbin/nologin -r -g glances glances
 mkdir -p /home/glances
-chown -R glances:glances /home/glances
 cp glances.service /etc/systemd/system/glances.service
 cp glances.conf /home/glances/.config/glances/glances.conf
 echo 'host='$HOST >> /home/glances/.config/glances/glances.conf
 echo 'port='$PORT >> /home/glances/.config/glances/glances.conf
 echo 'prefix='$PREFIX >> /home/glances/.config/glances/glances.conf
 echo 'labels='$LABELS >> /home/glances/.config/glances/glances.conf
-
+chown -R glances:glances /home/glances
 
 systemctl start glances
 systemctl enable glances
