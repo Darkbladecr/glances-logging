05c67d74ebece04b5eaa553c410d6b28c12ba0ed
Darkbladecr <smig88@gmail.com>
2020-04-15 10:06:25 +0100

    fix install bugs

--- setup.sh
+++ setup.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-$USER=darkbladecr
-$HOST=localhost
-$PORT=9091
-$PREFIX=glances
-$LABELS=src:glances
-# sudo su
+HOST=localhost
+PORT=9091
+PREFIX=glances
+LABELS=src:glances
+
 groupadd --system glances
 useradd -s /sbin/nologin -r -g glances glances
+mkdir -p /home/glances
+chown -R glances:glances /home/glances
 cp glances.service /etc/systemd/system/glances.service
-cp glances.conf /home/$USER/.config/glances/glances.conf
-echo 'host='$HOST >> /home/$USER/.config/glances/glances.conf
-echo 'port='$PORT >> /home/$USER/.config/glances/glances.conf
-echo 'prefix='$PREFIX >> /home/$USER/.config/glances/glances.conf
-echo 'labels='$LABELS >> /home/$USER/.config/glances/glances.conf
+cp glances.conf /home/glances/.config/glances/glances.conf
+echo 'host='$HOST >> /home/glances/.config/glances/glances.conf
+echo 'port='$PORT >> /home/glances/.config/glances/glances.conf
+echo 'prefix='$PREFIX >> /home/glances/.config/glances/glances.conf
+echo 'labels='$LABELS >> /home/glances/.config/glances/glances.conf
 
 
 systemctl start glances
